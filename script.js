182a6fffeae687795f8b1df52e389ab3bebe1ed2
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-05-22 20:22:35 -0300

    CALC

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const tela = document.getElementById("tela");
-function adicionar_na_tela(valor) {
-    tela.innerHTML += `${valor}`;
+const tela_div = document.getElementById("telinha");
+const previa_div = document.getElementById("previa")
+function adicionando(valor) {
+    tela_div.innerHTML += `${valor}`
 }
 function remover() {
-    let texto = tela.innerText
-    let sem_ultimo = texto.slice(0, -1);
-    tela.innerHTML = `${sem_ultimo}`
+    let remove_ultimo = tela_div.textContent.slice(0, -1);
+    tela_div.innerHTML = `${remove_ultimo}`
+}
+function resetar() {
+    tela_div.innerHTML = " "
+    previa_div.innerHTML = " "
 }
 
-function resultado() {
-    let texto = tela.innerText
-    console.log(Math.texto)
+
+
+
+let calculando = 0
+function sinais(valor) {
+    let valor_tela = tela_div.textContent
+    let valor_previa = previa_div.textContent
+    let splitString = valor_previa.split("");
+    let reverseArray = splitString.reverse();
+
+    if (valor === reverseArray[0]) {
+        reverseArray[0] = `${valor}`
+    }
+    console.log(reverseArray[0])
+    previa_div.innerHTML += `${valor_tela}${valor}`
+    tela_div.innerHTML = " "
 }
+
